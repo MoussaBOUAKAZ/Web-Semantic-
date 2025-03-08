abe9a5b22557d0aa2dd959b5cf41f8edfbe2f7c1
MoussaBOUAKAZ <bouakazmoussa0@gmail.com>
2025-03-08 02:16:57 +0100

    add query Structure component

--- web-semantic-app/src/App.js
+++ web-semantic-app/src/App.js
@@ -5,16 +5,39 @@ import SideBar from "./SideBar";
 import QueryIndex from "./QueryIndex";
 function App() {
   const [SideBarToggle, setSideBarToggle] = useState(true);
+  const [queryStructure, setQueryStructure] = useState("");
 
   const toggleSideBar = () => {
     setSideBarToggle((prev) => !prev);
   };
+  const [text, setText] = useState("Hello, this is default text!");
 
+  const handleChange = (event) => {
+    setQueryStructure(event.target.value);
+  };
+
+  const dispalyQuery = (Query) => {
+    setQueryStructure(Query);
+  };
   return (
     <div className="App">
       <Header showSideBar={toggleSideBar} />
-      <SideBar classes={`SideBar ${SideBarToggle ? "active" : ""}`} />
-     
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <SideBar
+          classes={`SideBar ${SideBarToggle ? "active" : ""}`}
+          showQueryStructure={dispalyQuery}
+        />
+        <div className="QuerySpace">
+          <textarea value={queryStructure} onChange={handleChange}></textarea>
+
+          <div className="QueryActions">
+            <button >
+              Run Query
+            </button>
+            <button onClick={() => setQueryStructure("")}>Reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- web-semantic-app/src/SideBar.js
+++ web-semantic-app/src/SideBar.js
@@ -2,36 +2,115 @@ import "./App.css";
 import SideBarItem from "./SideBarItem";
 import { useState } from "react";
 
-function SideBar({ classes }) {
+function SideBar({ classes, showQueryStructure }) {
   const [activeIndex, setActiveIndex] = useState(null);
 
-  const handleItemClick = (index) => {
+  const handleItemClick = (index, query) => {
     setActiveIndex(index);
+    showQueryStructure(query);
   };
 
   const items = [
-    { IconLink: null, Label: "Data Service" },
-    { IconLink: "importer.png", Label: "Import Graphe" },
-    { IconLink: "importer.png", Label: "Import 2" },
-    { IconLink: null, Label: "Query Tools" },
-    { IconLink: "openqueyIcon.png", Label: "Open Query" },
-    { IconLink: "readQueryIcon.png", Label: "SELECT Query" },
-    { IconLink: "askIcon.png", Label: "ASK Query" },
-    { IconLink: "constructIcon.png", Label: "CONSTRUCT Query" },
-    { IconLink: "describeIcon.png", Label: "DESCRIBE Query" },
-    { IconLink: "insertIcon.png", Label: "INSERT Query" },
-    { IconLink: "deleteIcon.png", Label: "DELETE Query" },
-    { IconLink: "deleteIcon.png", Label: "DELETE WHERE Query" },
-    { IconLink: "insertIcon.png", Label: "INSERT WHERE Query" },
-    { IconLink: "insertIcon.png", Label: "DELETE + INSERT Query" },
-    { IconLink: "insertIcon.png", Label: "INSERT INTO GRAPH Query" },
-    { IconLink: "deleteIcon.png", Label: "DELETE FROM GRAPH Query" },
-    { IconLink: "deleteIcon.png", Label: "DROP GRAPH Query" },
-    { IconLink: "filterIcon.png", Label: "FILTER Query" },
-    { IconLink: "optionaIcon.png", Label: "OPTIONAL  Query" },
-    { IconLink: "unionIcon.png", Label: "UNION  Query" },
-    { IconLink: "bindIcon.png", Label: "BIND  Query" },
-    { IconLink: "grouping.png", Label: "GROUP BY & COUNT  Query" },
+    { IconLink: null, Label: "Data Service", query: "" },
+    { IconLink: "importer.png", Label: "Import Graphe", query: "" },
+    { IconLink: "importer.png", Label: "Import 2", query: "" },
+    { IconLink: null, Label: "Query Tools", query: "" },
+    { IconLink: "openqueyIcon.png", Label: "Open Query", query: "" },
+    {
+      IconLink: "readQueryIcon.png",
+      Label: "SELECT Query",
+      query:
+        "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object } LIMIT 10",
+    },
+    {
+      IconLink: "askIcon.png",
+      Label: "ASK Query",
+      query: "ASK WHERE { ?s ?p ?o }",
+    },
+    {
+      IconLink: "constructIcon.png",
+      Label: "CONSTRUCT Query",
+      query: "CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o }",
+    },
+    {
+      IconLink: "describeIcon.png",
+      Label: "DESCRIBE Query",
+      query: "DESCRIBE <http://example.com/resource>",
+    },
+    {
+      IconLink: "insertIcon.png",
+      Label: "INSERT Query",
+      query:
+        "INSERT DATA { <http://example.com/person1> <http://xmlns.com/foaf/0.1/name> 'Alice' }",
+    },
+    {
+      IconLink: "deleteIcon.png",
+      Label: "DELETE Query",
+      query: "DELETE WHERE { ?s ?p ?o }",
+    },
+    {
+      IconLink: "deleteIcon.png",
+      Label: "DELETE WHERE Query",
+      query: "DELETE WHERE { ?s ?p 'OldValue' }",
+    },
+    {
+      IconLink: "insertIcon.png",
+      Label: "INSERT WHERE Query",
+      query:
+        "INSERT { ?s <http://example.com/age> 30 } WHERE { ?s a <http://example.com/Person> }",
+    },
+    {
+      IconLink: "insertIcon.png",
+      Label: "DELETE + INSERT Query",
+      query:
+        "DELETE { ?s <http://example.com/age> 25 } INSERT { ?s <http://example.com/age> 26 } WHERE { ?s <http://example.com/age> 25 }",
+    },
+    {
+      IconLink: "insertIcon.png",
+      Label: "INSERT INTO GRAPH Query",
+      query:
+        "INSERT DATA { GRAPH <http://example.com/graph> { <http://example.com/person1> <http://xmlns.com/foaf/0.1/name> 'Alice' } }",
+    },
+    {
+      IconLink: "deleteIcon.png",
+      Label: "DELETE FROM GRAPH Query",
+      query: "DELETE WHERE { GRAPH <http://example.com/graph> { ?s ?p ?o } }",
+    },
+    {
+      IconLink: "deleteIcon.png",
+      Label: "DROP GRAPH Query",
+      query: "DROP GRAPH <http://example.com/graph>",
+    },
+    {
+      IconLink: "filterIcon.png",
+      Label: "FILTER Query",
+      query:
+        "SELECT ?s WHERE { ?s <http://example.com/age> ?age FILTER(?age > 25) }",
+    },
+    {
+      IconLink: "optionaIcon.png",
+      Label: "OPTIONAL Query",
+      query:
+        "SELECT ?s ?name ?email WHERE { ?s <http://xmlns.com/foaf/0.1/name> ?name OPTIONAL { ?s <http://xmlns.com/foaf/0.1/mbox> ?email } }",
+    },
+    {
+      IconLink: "unionIcon.png",
+      Label: "UNION Query",
+      query:
+        "SELECT ?s WHERE { { ?s <http://example.com/type> <http://example.com/Person> } UNION { ?s <http://example.com/type> <http://example.com/Organization> } }",
+    },
+    {
+      IconLink: "bindIcon.png",
+      Label: "BIND Query",
+      query:
+        "SELECT ?s ?ageInDays WHERE { ?s 'URI' ?age BIND(?age * 365 AS ?ageInDays) }",
+    },
+    {
+      IconLink: "grouping.png",
+      Label: "GROUP BY & COUNT Query",
+      query:
+        "SELECT ?type (COUNT(?s) AS ?count) WHERE { ?s a ?type } GROUP BY ?type",
+    },
   ];
 
   return (
@@ -42,7 +121,7 @@ function SideBar({ classes }) {
           IconLink={item.IconLink}
           Label={item.Label}
           isActive={activeIndex === index}
-          onClick={() => handleItemClick(index)}
+          onClick={() => handleItemClick(index, item.query)}
         />
       ))}
     </div>
